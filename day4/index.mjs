3982b434dcd37a0ec6fd07a79d91310ef0341b7a
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-12-20 16:36:03 +0900

    Day 4:  Part 1 (~0:30:00) 🫳

---
+++ day4/index.mjs
@@ -0,0 +1,29 @@
+import data from "./data.mjs";
+
+const rows = data.split("\n");
+
+const schedules = rows.map(row => row.split(",").map(element => element.split("-")));
+
+const isOverlapping = (schedule) => {
+  const a = schedule[0];
+  const b = schedule[1];
+  const aStart = parseInt(a[0]);
+  const bStart = parseInt(b[0]);
+
+  if (aStart > bStart) return isOverlapping([b, a]);
+
+  const aEnd = parseInt(a[1]);
+  const bEnd = parseInt(b[1]);
+
+  if (aStart === bStart) return true;
+  return bEnd <= aEnd;
+}
+
+const part1 = schedules.reduce((acc, schedule) => {
+  if (isOverlapping(schedule)) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(part1);
