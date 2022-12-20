d1922b0286165067592f2c7e7c1c475a4e69c992
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-12-20 21:30:42 +0900

    Day 5: Part 1 ( 0:22:57) 👌

---
+++ day5/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const data = require('./data.js').data;
+
+let crates = [
+  [],
+  ['D', 'L', 'J', 'R', 'V', 'G', 'F'],
+  ['T', 'P', 'M', 'B', 'V', 'R', 'J', 'S'],
+  ['V', 'H', 'M', 'F', 'D', 'G', 'P', 'C'],
+  ['M', 'D', 'P', 'N', 'G', 'Q'],
+  ['J', 'L', 'H', 'N', 'F'],
+  ['N', 'F', 'V', 'Q', 'D', 'G', 'T', 'Z'],
+  ['F', 'D', 'B', 'L'],
+  ['M', 'J', 'B', 'S', 'V', 'D', 'N'],
+  ['G', 'L', 'D']
+]
+
+let lines = data.split("\n").map(line => line.trim().split(" "));
+
+const operations = lines.map(line => {
+  return [line[1], line[3], line[5]].map(Number);
+})
+
+operations.forEach(operation => {
+  const [amount, from, to] = operation;
+
+  for (let i = 0; i < amount; i++) {
+    crates[to].push(crates[from].pop())
+  }
+})
+
+console.log(crates.map(crate => crate.reverse()[0]).join(""))
