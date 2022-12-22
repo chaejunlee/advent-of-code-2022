aba175d886ce47bde89af88c69651c7ac1a1a20c
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-12-23 01:57:10 +0900

    Day 6: Part 1 (00:14:20) 👌

---
+++ day6/index.js
@@ -0,0 +1,21 @@
+const data = require('./data.js').data;
+
+let result = 0;
+
+for (let i = 0; i < data.length; i++) {
+  let subroutine = {};
+  for (let j = 0; j < 4; j++) {
+    if (subroutine[data[i + j]] === undefined) subroutine[data[i + j]] = 1;
+    else subroutine[data[i + j]]++;
+  }
+  let checker = 0;
+  for (const key in subroutine) {
+    if (subroutine[key] > 1) break;
+    checker++;
+  }
+  console.log(subroutine, checker);
+  if (checker === 4) result = i + 3;
+  if (result !== 0) break;
+}
+// 00:14:20
+console.log(result);
