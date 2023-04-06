3a2ce154ff70c28e587cb358001fdcfb73c9b973
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-04-07 03:44:25 +0900

    Day 9: Part 2 (1:24:41)

--- day9/index.js
+++ day9/index.js
@@ -2,7 +2,6 @@ const data = require('./data.js');
 
 /**
  * if head moves, tail has to move too.
- * 
  */
 
 const lines = data.split('\n');
@@ -18,26 +17,45 @@ const dir = {
   'R': { x: 1, y: 0 }
 }
 
-let head = { y: 0, x: 0 };
-let tail = { y: 0, x: 0 };
+let knots = [
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+  { y: 0, x: 0 },
+];
 let checker = new Set();
-checker.add(JSON.stringify(tail));
+checker.add(JSON.stringify(knots[9]));
 
 for (const dir_char of moves) {
   const direction = dir[dir_char.direction];
   for (let i = 0; i < dir_char.move; i++) {
-    const prevHead = { ...head };
-    head.x += direction.x;
-    head.y += direction.y;
+    knots[0].x += direction.x;
+    knots[0].y += direction.y;
 
-    const yDiff = Math.abs(head.y - tail.y);
-    const xDiff = Math.abs(head.x - tail.x);
+    for (let knot = 1; knot <= 9; knot++) {
+      const yDiff = (knots[knot - 1].y - knots[knot].y);
+      const xDiff = (knots[knot - 1].x - knots[knot].x);
 
-    if (yDiff <= 1 && xDiff <= 1) {
-      continue;
+      if (Math.abs(yDiff) <= 1 && Math.abs(xDiff) <= 1) {
+        continue;
+      }
+
+      const yMove = yDiff == 0 ? 0 : yDiff < 0 ? -1 : 1;
+      const xMove = xDiff == 0 ? 0 : xDiff < 0 ? -1 : 1;
+
+      // calculating this part was awful
+      // I thought it was just copy pasting previous points, but it was not.
+      // need to do some serious calculations
+      knots[knot].x += xMove;
+      knots[knot].y += yMove;
     }
-    tail = prevHead;
-    checker.add(JSON.stringify(tail));
+    checker.add(JSON.stringify(knots[9]));
   }
 }
 
